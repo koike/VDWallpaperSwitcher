f31869b67b269da5988fb7bb770b77c411a384f9
Kirin MORINO <info@o8o.jp>
2016-05-20 00:06:19 +0900

    :up: Updated VDWallpaperSwitcher/MainWindow.xaml.cs

--- VDWallpaperSwitcher/MainWindow.xaml.cs
+++ VDWallpaperSwitcher/MainWindow.xaml.cs
@@ -7,39 +7,32 @@ using System.Runtime.InteropServices;
 
 namespace VDWallpaperSwitcher
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
     public partial class MainWindow : Window
     {
-        const int SPI_SETDESKWALLPAPER = 20;
-        const int SPIF_UPDATEINIFILE = 0x01;
-        const int SPIF_SENDWININICHANGE = 0x02;
+        private const int SPI_SETDESKWALLPAPER = 20,
+                          SPIF_UPDATEINIFILE = 0x01,
+                          SPIF_SENDWININICHANGE = 0x02;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
         public MainWindow()
         {
-            VirtualDesktop.CurrentChanged += this.CurrentChanged;
-            this.InitializeComponent();
-        }
-
-        private void CurrentChanged(object sender, VirtualDesktopChangedEventArgs e)
-        {
-            var index = Array.IndexOf(VirtualDesktop.GetDesktops(),
-                VirtualDesktop.GetDesktops().First(i => i.Id == VirtualDesktop.Current.Id));
-
-            if (index > -1)
+            VirtualDesktop.CurrentChanged += (sender, e) =>
             {
-                var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\img", "*", SearchOption.AllDirectories);
-                var path = files[index % files.Length];
+                var desktops = VirtualDesktop.GetDesktops();
+                var index = Array.IndexOf(desktops, desktops.First(i => i.Id == VirtualDesktop.Current.Id));
 
-                if (File.Exists(path))
+                if (index > -1)
                 {
-                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                    var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\img", "*",
+                        SearchOption.AllDirectories);
+                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0,
+                        files[index % files.Length], SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
                 }
-            }
+            };
+
+            this.InitializeComponent();
         }
     }
 }
